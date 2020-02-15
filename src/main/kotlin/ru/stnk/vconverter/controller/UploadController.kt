e8b63102376364ed4be8863ff43d16e8fefb50d1
AleksandrSTNK <aleksandr.stnk@gmail.com>
2020-02-15 21:31:01 +0300

    " add function checkAndSave "

--- src/main/kotlin/ru/stnk/vconverter/controller/UploadController.kt
+++ src/main/kotlin/ru/stnk/vconverter/controller/UploadController.kt
@@ -6,18 +6,18 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
-import ru.stnk.vconverter.storage.StorageService
+import ru.stnk.vconverter.service.UploadControllerService
 
 
 @RestController
 class UploadController (
-        private val storageService: StorageService
+        private val uploadService: UploadControllerService
 ) {
 
     @PostMapping("/upload")
     fun handleFileUpload(@RequestParam("file") file: MultipartFile) : ResponseEntity<out Any> {
 
-        val name: String = storageService.store(file)
+        val name: String = uploadService.checkAndSaveFile(file)
 
         return ResponseEntity(mapOf("id" to name), HttpStatus.OK)
 
--- src/main/kotlin/ru/stnk/vconverter/service/UploadControllerService.kt
+++ src/main/kotlin/ru/stnk/vconverter/service/UploadControllerService.kt
@@ -1,4 +1,20 @@
 package ru.stnk.vconverter.service
 
-class UploadControllerService {
+import org.springframework.stereotype.Service
+import org.springframework.util.StringUtils
+import org.springframework.web.multipart.MultipartFile
+import ru.stnk.vconverter.storage.StorageService
+import ru.stnk.vconverter.storage.exception.FileExtentionException
+
+
+@Service
+class UploadControllerService (
+        private val storageService: StorageService
+) {
+    fun checkAndSaveFile (file: MultipartFile): String {
+        when (StringUtils.getFilenameExtension(file.originalFilename)) {
+            "mov","avi","wmv","flv","3gp","mp4","mpg" -> return storageService.store(file)
+            else -> throw FileExtentionException("Недопустимый формат файла")
+        }
+    }
 }
